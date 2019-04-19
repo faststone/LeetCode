194cc6a3df150a5a41a0b2d83d3e136945c6e770
huwenxiao <huwenxiao@smarttof.com>
2019-04-19 10:55:56 +0800

    update

--- main.cpp
+++ main.cpp
@@ -27,36 +27,39 @@ bool isBadVersion(int version);
 
 class Solution {
 public:
-    int firstBadVersion(int n) {
-        int low = 1;
-        int high = n;
-        int midIndex = 0;
-        while (low <= high)
+
+    string newStr = "1";
+    string countAndSay(int n) {
+        if (n == 1)
         {
-            midIndex = low + (high - low) / 2;
-            if (isBadVersion(midIndex))
+            return newStr;
+        }
+        string last;
+        string lastString = newStr;
+        newStr.clear();
+        int number = 1;
+        for (int i = 0; i < lastString.size(); i++)
+        {
+            if ( i == lastString.size() -1 )
             {
-                high = midIndex - 1;
+                newStr += to_string(number);
+                newStr += lastString[i];
+            }
+            else if (lastString[i] == lastString[i+1])
+            {
+                number++;
             }
             else
             {
-                low = midIndex + 1;
+                newStr += to_string(number);
+                newStr += lastString[i];
+                number = 1;
             }
         }
-        return low;
+        return countAndSay(--n);
     }
 };
-// class Solution {
-// public:
-//     bool isValidBST(TreeNode *root) {
-//         return isValidBST(root, LONG_MIN, LONG_MAX);
-//     }
-//     bool isValidBST(TreeNode *root, long mn, long mx) {
-//         if (!root) return true;
-//         if (root->val <= mn || root->val >= mx) return false;
-//         return isValidBST(root->left, mn, root->val) && isValidBST(root->right, root->val, mx);
-//     }
-// };
+
 
 int main()
 { 
@@ -73,7 +76,7 @@ int main()
     //vector<int> str =
     vector<int> a = { 1, 2, 3, 4 };
     vector<int> b = { 2, 4, 5, 6 };
-    S.firstBadVersion(5);
+    std::cout << S.countAndSay(6);
 //      for (int i = 0; i < str.size(); i++)
 //      {
 //          std::cout << str[i] << ' ";
