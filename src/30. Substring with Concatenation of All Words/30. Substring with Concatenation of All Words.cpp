a17fb428251b925adc204bc119105f0c5a195dd7
huwenxiao <huwenxiao@smarttof.com>
2022-05-12 10:50:37 +0800

    add 30. Substring with Concatenation of All Words

---
+++ src/30. Substring with Concatenation of All Words/30. Substring with Concatenation of All Words.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <ctime>
+#include <set>
+#include <map>
+#include <queue>
+#include "ListNode.h"
+#include "BinTree.h"
+
+using namespace std;
+#define null 0
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+*/
+
+class Solution {
+public:
+    void backtracking(vector<string> &strv, vector<string>& returnStr, int first, int len)
+    {
+        if (first == len)
+        {
+            string s;
+            for (auto& x : strv)
+            {
+                s += x;
+            }
+            returnStr.push_back(s);
+            return;
+        }
+
+        for (int i = first; i < len; ++i)
+        {
+            swap(strv[i], strv[first]);
+            backtracking(strv, returnStr, first +1, len);
+            swap(strv[i], strv[first]);
+        }
+    }
+
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<string> returnStr;
+        set<int> v;
+        vector<int> find;
+        backtracking(words, returnStr, 0, words.size());
+        for (auto var : returnStr)
+        {
+            string::size_type position = 0;
+            while ((position = s.find(var, position)) != string::npos)
+            {
+                v.emplace(position);
+                position += var.size();
+            }
+        }
+        for (auto var : v)
+        {
+            find.push_back(var);
+        }
+        return find;
+    }
+};
+
+
+
+int main()
+{
+    vector<int> price = { 7, 1, 5, 3, 6, 4 };
+    vector<int> price2 = { 7,6,4,3,1 };
+    string s = "pjzkrkevzztxductzzxmxsvwjkxpvukmfjywwetvfnujhweiybwvvsrfequzkhossmootkmyxgjgfordrpapjuunmqnxxdrqrfgkrsjqbszgiqlcfnrpjlcwdrvbumtotzylshdvccdmsqoadfrpsvnwpizlwszrtyclhgilklydbmfhuywotjmktnwrfvizvnmfvvqfiokkdprznnnjycttprkxpuykhmpchiksyucbmtabiqkisgbhxngmhezrrqvayfsxauampdpxtafniiwfvdufhtwajrbkxtjzqjnfocdhekumttuqwovfjrgulhekcpjszyynadxhnttgmnxkduqmmyhzfnjhducesctufqbumxbamalqudeibljgbspeotkgvddcwgxidaiqcvgwykhbysjzlzfbupkqunuqtraxrlptivshhbihtsigtpipguhbhctcvubnhqipncyxfjebdnjyetnlnvmuxhzsdahkrscewabejifmxombiamxvauuitoltyymsarqcuuoezcbqpdaprxmsrickwpgwpsoplhugbikbkotzrtqkscekkgwjycfnvwfgdzogjzjvpcvixnsqsxacfwndzvrwrycwxrcismdhqapoojegggkocyrdtkzmiekhxoppctytvphjynrhtcvxcobxbcjjivtfjiwmduhzjokkbctweqtigwfhzorjlkpuuliaipbtfldinyetoybvugevwvhhhweejogrghllsouipabfafcxnhukcbtmxzshoyyufjhzadhrelweszbfgwpkzlwxkogyogutscvuhcllphshivnoteztpxsaoaacgxyaztuixhunrowzljqfqrahosheukhahhbiaxqzfmmwcjxountkevsvpbzjnilwpoermxrtlfroqoclexxisrdhvfsindffslyekrzwzqkpeocilatftymodgztjgybtyheqgcpwogdcjlnlesefgvimwbxcbzvaibspdjnrpqtyeilkcspknyylbwndvkffmzuriilxagyerjptbgeqgebiaqnvdubrtxibhvakcyotkfonmseszhczapxdlauexehhaireihxsplgdgmxfvaevrbadbwjbdrkfbbjjkgcztkcbwagtcnrtqryuqixtzhaakjlurnumzyovawrcjiwabuwretmdamfkxrgqgcdgbrdbnugzecbgyxxdqmisaqcyjkqrntxqmdrczxbebemcblftxplafnyoxqimkhcykwamvdsxjezkpgdpvopddptdfbprjustquhlazkjfluxrzopqdstulybnqvyknrchbphcarknnhhovweaqawdyxsqsqahkepluypwrzjegqtdoxfgzdkydeoxvrfhxusrujnmjzqrrlxglcmkiykldbiasnhrjbjekystzilrwkzhontwmehrfsrzfaqrbbxncphbzuuxeteshyrveamjsfiaharkcqxefghgceeixkdgkuboupxnwhnfigpkwnqdvzlydpidcljmflbccarbiegsmweklwngvygbqpescpeichmfidgsjmkvkofvkuehsmkkbocgejoiqcnafvuokelwuqsgkyoekaroptuvekfvmtxtqshcwsztkrzwrpabqrrhnlerxjojemcxel";
+    vector<string> words = {"dhvf","sind","ffsl","yekr","zwzq","kpeo","cila","tfty","modg","ztjg","ybty","heqg","cpwo","gdcj","lnle","sefg","vimw","bxcb"};
+    Solution S;
+    vector<int> v =S.findSubstring(s, words);
+    for (auto var : v)
+    {
+        cout << var << endl;
+    }
+    clock_t startTime = clock();
+    clock_t endTime = clock();
+    cout << "sortName: " << double(endTime - startTime) / CLOCKS_PER_SEC << " s" << endl;
+    return 0;
+}
